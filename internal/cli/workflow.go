29d487b23191956c6725aff4cad381277e886737
Gustavo Bertoi <gustavobertoi@outlook.com>
2025-04-21 22:36:51 +0200

    refactor(cli): updating internal cli

--- internal/cli/workflow.go
+++ internal/cli/workflow.go
@@ -2,7 +2,8 @@ package cli
 
 import (
 	"fmt"
-	"github.com/open-source-cloud/fuse/internal/graph"
+
+	"github.com/open-source-cloud/fuse/internal/graph/memory"
 	"github.com/open-source-cloud/fuse/internal/providers/debug"
 	"github.com/open-source-cloud/fuse/internal/providers/logic"
 	"github.com/open-source-cloud/fuse/internal/workflow"
@@ -13,35 +14,51 @@ import (
 // Workflow example command
 var workflowCmd = &cobra.Command{
 	Use:   "workflow",
-	Short: "Workflow example",
-	RunE:  workflowExampleRunner,
+	Short: "Workflow runner",
+	RunE:  workflowRunner,
 }
 
 // Workflow example runner
-func workflowExampleRunner(_ *cobra.Command, _ []string) error {
+func workflowRunner(_ *cobra.Command, _ []string) error {
 	engine := workflow.NewEngine()
 
 	debugProvider := debug.NewNodeProvider()
 	logicProvider := logic.NewNodeProvider()
 
-	rootNodeConfig := graph.NewNodeConfig()
-	rootNode := graph.NewNode(uuid.V7(), debugProvider.Nodes()[0], rootNodeConfig)
-	newGraph := graph.NewGraph(rootNode)
+	nilNode, err := debugProvider.GetNode(debug.NilNodeID)
+	if err != nil {
+		return err
+	}
 
-	randNode1Config := graph.NewNodeConfig()
-	randNode1 := graph.NewNode(uuid.V7(), logicProvider.Nodes()[1], randNode1Config)
+	rootNodeConfig := memory.NewNodeConfig()
+	rootNode := memory.NewNode(uuid.V7(), nilNode, rootNodeConfig)
+	newGraph := memory.NewGraph(rootNode)
+
+	randNode, err := logicProvider.GetNode(logic.RandNodeID)
+	if err != nil {
+		return err
+	}
+
+	randNode1Config := memory.NewNodeConfig()
+	randNode1 := memory.NewNode(uuid.V7(), randNode, randNode1Config)
 	randNodeEdgeID1 := uuid.V7()
 	newGraph.AddNode(rootNode.ID(), randNodeEdgeID1, randNode1)
-	randNode2Config := graph.NewNodeConfig()
+
+	randNode2Config := memory.NewNodeConfig()
 	randNodeEdgeID2 := uuid.V7()
-	randNode2 := graph.NewNode(uuid.V7(), logicProvider.Nodes()[1], randNode2Config)
+	randNode2 := memory.NewNode(uuid.V7(), randNode, randNode2Config)
 	newGraph.AddNode(rootNode.ID(), randNodeEdgeID2, randNode2)
 
-	sumNodeConfig := graph.NewNodeConfig()
+	sumNode, err := logicProvider.GetNode(logic.SumNodeID)
+	if err != nil {
+		return err
+	}
+
+	sumNodeConfig := memory.NewNodeConfig()
 	sumNodeConfig.AddInputMapping(fmt.Sprintf("edge[%s]", randNodeEdgeID1), "rand", "values")
 	sumNodeConfig.AddInputMapping(fmt.Sprintf("edge[%s]", randNodeEdgeID2), "rand", "values")
-	sumNode := graph.NewNode(uuid.V7(), logicProvider.Nodes()[0], sumNodeConfig)
-	newGraph.AddNodeMultipleParents([]string{randNode1.ID(), randNode2.ID()}, uuid.V7(), sumNode)
+	sumNodeWorkflow := memory.NewNode(uuid.V7(), sumNode, sumNodeConfig)
+	newGraph.AddNodeMultipleParents([]string{randNode1.ID(), randNode2.ID()}, uuid.V7(), sumNodeWorkflow)
 
 	engine.Start()
 
